afecb440f04a624a92e8626f6b1f0b50cd54d493
Matthew Erbs <matthew.erbs@merbla.com>
2015-06-08 19:59:26 +1000

    clean up

--- src/octopusdeploy-events-modinput/README/inputs.conf.spec
+++
@@ -1,5 +0,0 @@
-[octopus_deploy://<octopusDeployHost>]
-*Generates events of from an Octopus Deploy instance.
-
-octopusDeployHost = <value>
-apikey = <value>
--- src/package.sh
+++ src/package.sh
@@ -1,9 +1,14 @@
-rm octopusdeploy-events-modinput.tar.gz
-tar -czf octopusdeploy-events-modinput.tar.gz octopusdeploy-events-modinput/
 
-rm octopusdeploy-deployments-modinput.tar.gz
-tar -czf octopusdeploy-deployments-modinput.tar.gz octopusdeploy-deployments-modinput/
+rm octopusdeploy-modinput.tar.gz
+tar -czf octopusdeploy-modinput.tar.gz octopusdeploy-modinput/
 
-rm octopusdeploy-tasks-modinput.tar.gz
-tar -czf octopusdeploy-tasks-modinput.tar.gz octopusdeploy-tasks-modinput/
 
+# rm octopusdeploy-events-modinput.tar.gz
+# tar -czf octopusdeploy-events-modinput.tar.gz octopusdeploy-events-modinput/
+
+
+# rm octopusdeploy-deployments-modinput.tar.gz
+# tar -czf octopusdeploy-deployments-modinput.tar.gz octopusdeploy-deployments-modinput/
+
+# rm octopusdeploy-tasks-modinput.tar.gz
+# tar -czf octopusdeploy-tasks-modinput.tar.gz octopusdeploy-tasks-modinput/
