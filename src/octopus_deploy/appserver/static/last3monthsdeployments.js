eb1a61dc7cbaba23b85491a55e88bee0433988e4
Matthew Erbs <matthew.erbs@merbla.com>
2015-07-10 08:22:32 +1000

    Refactoring on dashboard ideas

--- src/octopus_deploy/appserver/static/last3monthsdeployments.js
+++ src/octopus_deploy/appserver/static/last3monthsdeployments.js
@@ -5,18 +5,30 @@ require([
   'splunkjs/mvc',
   'splunkjs/mvc/d3chart/d3chartview',
   'util/moment',
-  'splunkjs/mvc/simplexml/ready!'
-], function(_, $, utils, mvc, D3ChartView, moment) {
+  "splunkjs/ready!",
+  "splunkjs/mvc/searchmanager",
+], function(_, $, utils, mvc, D3ChartView, moment, ready, searchManager) {
 
+  var mainSearch = new searchManager({
+    id: "last3monthsdeploymentsSearch",
+    search: "sourcetype=octopus:deployment | rename Id as DeploymentId | join EnvironmentId [ search sourcetype=octopus:environment | rename Id as EnvironmentId, Name as EnvironmentName ] | timechart count(DeploymentId) span=1month by EnvironmentName",
 
-  var tableManagerid = mvc.Components.get('last3monthsdeploymentsTable').settings.get('managerid');
+  });
+
+  // var tableManagerid = mvc.Components.get('last3monthsdeploymentsTable').settings.get('managerid');
 
   var chart = new D3ChartView({
     "id": "last3monthsdeployments",
-    "managerid": tableManagerid,
+    //"managerid": tableManagerid,
+    "managerid": mainSearch.id,
     "type": "linePlusBarChart",
     "el": $('#last3monthsdeployments')
   }).render();
+
+
+  // Total Deployments one side, Total Releases the other
+
+
 
   chart.settings.set("setup", function(chart) {
     chart.margin({
@@ -26,14 +38,42 @@ require([
       bottom: 70
     });
 
-
-    chart.xAxis.axisLabel('Yes');
+    chart.xAxis.axisLabel('');
 
     chart.xAxis.tickFormat(function(d) {
-      var format = d3.time.format("%m-%y");
+      var format = d3.time.format("%B");
       return format(new Date(d));
     });
 
     chart.xAxis.rotateLabels(45);
   });
+
+  // var SearchManager = require("splunkjs/mvc/searchmanager");
+  //
+  // var mainSearch = new SearchManager({
+  //   id: "last3monthsdeploymentsSearch",
+  //   search: "sourcetype=octopus:deployment | rename Id as DeploymentId | join EnvironmentId [ search sourcetype=octopus:environment | rename Id as EnvironmentId, Name as EnvironmentName ] | timechart count(DeploymentId) span=1month by EnvironmentName",
+  //
+  // });
+
+
+  // var mainSearch = splunkjs.mvc.Components.getInstance(tableManagerid);
+  // var mai  nSearch = splunkjs.mvc.Components.getInstance("last3monthsdeploymentsSearch");
+  var myResults = mainSearch.data("preview", {});
+
+  myResults.on("data", function() {
+    // The full data object
+    console.log(myResults.data());
+
+    // Indicates whether the results model has data
+    console.log("Has data? ", myResults.hasData());
+
+    // The results rows
+    console.log("Data (rows): ", myResults.data().rows);
+
+    // The Backbone collection
+    console.log("Backbone collection: ", myResults.collection());
+  });
+
+
 });
