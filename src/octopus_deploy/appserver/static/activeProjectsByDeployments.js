2e9cad0c6cb44df8ddc3f00279376e90bf2a1aff
Matthew Erbs <matthew.erbs@merbla.com>
2015-07-11 21:51:00 +1000

    removed old files

--- src/octopus_deploy/appserver/static/activeProjectsByDeployments.js
+++ src/octopus_deploy/appserver/static/activeProjectsByDeployments.js
@@ -1,6 +1,6 @@
 require.config({
   paths: {
-    lodash: '//cdnjs.cloudflare.com/ajax/libs/lodash.js/3.10.0/lodash.min',
+    lodash: '../app/octopus_deploy/bower_components/lodash/lodash.min',
     d3v3: '../app/octopus_deploy/bower_components/d3/d3.min',
     nvd3: '../app/octopus_deploy/bower_components/nvd3/build/nv.d3.min',
   },
@@ -38,7 +38,7 @@ require([
 
   var mainSearch = new searchManager({
     id: "activeProjectsByDeploymentsSearch",
-    search: "sourcetype=octopus:deployment | rename Id as DeploymentId | join EnvironmentId [ search sourcetype=octopus:environment | rename Id as EnvironmentId, Name as EnvironmentName ] | join ProjectId [ search sourcetype=octopus:project | rename Id as ProjectId, Name as ProjectName ] | timechart count(DeploymentId)  span=1day by ProjectName",
+      search: "sourcetype=octopus:deployment | rename Id as DeploymentId | join EnvironmentId [ search sourcetype=octopus:environment | rename Id as EnvironmentId, Name as EnvironmentName ] | join ProjectId [ search sourcetype=octopus:project | rename Id as ProjectId, Name as ProjectName ]  | timechart count(DeploymentId)  span=1day by ProjectName",
   });
 
   var results = mainSearch.data("preview", {});
@@ -52,7 +52,7 @@ require([
         return !i.indexOf("_") == 0;
       });
 
-       var d = _.pluck(results.collection().models, 'attributes');
+      var data = _.pluck(results.collection().models, 'attributes');
 
       _.each(seriesFields, function(f) {
         var item = {};
@@ -62,92 +62,65 @@ require([
       })
 
       _.each(series, function(s) {
-
-        _.each(d, function(item) {
-
-          // var t = _.propertyOf(item)("_time");
-          // var val = _.propertyOf(item)(s.key);
+        _.each(data, function(item) {
 
           var t = item["_time"];
           var val = item[s.key];
 
           var i = [];
-
           i.push(new Date(t));
           i.push(parseInt(val));
 
           s.values.push(i);
-
         });
       });
 
-      console.log(series);
+      var colors = d3.scale.category20();
 
+      var keyColor = function(d, i) {
+        return colors(d.key)
+      };
 
+      var chart;
 
-
-    var colors = d3.scale.category20();
-
-    var keyColor = function(d, i) {return colors(d.key)};
-
-    var chart;
-
-    nv.addGraph(function() {
-
-
+      nv.addGraph(function() {
         chart = nv.models.stackedAreaChart()
-            .useInteractiveGuideline(true)
-            .x(function(d) {
-                return d[0];
-            })
-            .y(function(d) {
-                //return 100;
-                return d[1];
-            })
-            //.controlLabels({stacked: "Stacked"})
-            .color(keyColor)
-            .duration(300);
+          .useInteractiveGuideline(true)
+          .x(function(d) {
+            return d[0];
+          })
+          .y(function(d) {
+            return d[1];
+          })
+          .color(keyColor)
+          .duration(300);
 
-        // chart.xAxis.tickFormat(function(d) {
-        //     return d3.time.format('%b')(d))
-        // });
-//d3.format("d")(20);
-        chart.yAxis.tickFormat(function(d){
-        var asd=
-            d3.format('d')(d);
-
-return asd
+        chart.yAxis.tickFormat(function(d) {
+          return d3.format('d')(d);
         });
 
+        chart.xAxis.tickFormat(function(d) {
+          return d3.time.format('%w')(new Date(d))
+        });
 
-
-        chart.xAxis.tickFormat(function(d) { return d3.time.format('%x')(new Date(d)) });
-
-
-//   var dialAngle = ((360/subComponentsCount)*(parseInt(subComponentId.charAt(subComponentId.length-1))))-offsetAngle;
-
+        chart.legend.vers('furious');
 
         d3.select('#activeProjectsByDeploymentsChart')
-            .datum(series)
-            .transition()
-            .duration(1000)
-            .call(chart)
-            .each('start', function() {
-                setTimeout(function() {
-                    d3.selectAll('#activeProjectsByDeploymentsChart *').each(function() {
-                        if(this.__transition__)
-                            this.__transition__.duration = 1;
-                    })
-                }, 0)
-            });
-
-
-
-
+          .datum(series)
+          .transition()
+          .duration(1000)
+          .call(chart)
+          .each('start', function() {
+            setTimeout(function() {
+              d3.selectAll('#activeProjectsByDeploymentsChart *').each(function() {
+                if (this.__transition__)
+                  this.__transition__.duration = 1;
+              })
+            }, 0)
+          });
         nv.utils.windowResize(chart.update);
         return chart;
-    });
-
+      });
 
     }
   });
