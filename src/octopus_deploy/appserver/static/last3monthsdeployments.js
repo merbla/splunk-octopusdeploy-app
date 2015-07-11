6e633a4175c24d28d9bd5bda277970a8de674441
Matthew Erbs <matthew.erbs@merbla.com>
2015-07-11 22:05:18 +1000

    Second dashboard graph

--- src/octopus_deploy/appserver/static/last3monthsdeployments.js
+++ src/octopus_deploy/appserver/static/last3monthsdeployments.js
@@ -1,50 +1,128 @@
+require.config({
+  paths: {
+    lodash: '../app/octopus_deploy/bower_components/lodash/lodash.min',
+    d3v3: '../app/octopus_deploy/bower_components/d3/d3.min',
+    nvd3: '../app/octopus_deploy/bower_components/nvd3/build/nv.d3.min',
+  },
+  shim: {
+    lodash: {
+      deps: []
+    },
+    d3v3: {
+      deps: []
+    },
+    nvd3: {
+      deps: ['d3v3']
+    }
+  }
+});
+
+// workaround for nvd3 using global d3
+define('d3.global', ['d3v3'], function(_) {
+  d3 = _;
+});
+
 require([
   'underscore',
-  'jquery',
-  'splunkjs/mvc/utils',
-  'splunkjs/mvc',
-  'splunkjs/mvc/d3chart/d3chartview',
   'util/moment',
   "splunkjs/ready!",
+  "splunkjs/mvc/simplexml/ready!",
   "splunkjs/mvc/searchmanager",
-], function(_, $, utils, mvc, D3ChartView, moment, ready, searchManager) {
+  "d3v3",
+  "nvd3",
+  "lodash"
+], function(_, moment, ready, simpleXmlReady, searchManager, d3, nv1, __) {
+
+  //Load v3
+  require("nvd3");
 
   var mainSearch = new searchManager({
-    id: "last3monthsdeploymentsSearch",
-    search: "sourcetype=octopus:deployment | rename Id as DeploymentId | join EnvironmentId [ search sourcetype=octopus:environment | rename Id as EnvironmentId, Name as EnvironmentName ] | timechart count(DeploymentId) span=1month by EnvironmentName",
-
+    id: "last3MonthDeployment",
+      search: "sourcetype=octopus:deployment | rename Id as DeploymentId | join EnvironmentId [ search sourcetype=octopus:environment | rename Id as EnvironmentId, Name as EnvironmentName ] | join ProjectId [ search sourcetype=octopus:project | rename Id as ProjectId, Name as ProjectName ]  | timechart count(DeploymentId)  span=1day by ProjectName",
   });
 
-  var chart = new D3ChartView({
-    "id": "last3monthsdeployments",
-    //"managerid": tableManagerid,
-    "managerid": mainSearch.id,
-    "type": "stackedAreaChart",
-    "el": $('#last3monthsdeployments')
-  }).render();
+  var results = mainSearch.data("preview", {});
 
-  chart.settings.set("setup", function(chart) {
-    chart.margin({
-      right: 100,
-      left: 80,
-      top: 70,
-      bottom: 70
-    });
+  results.on("data", function() {
+    if (results.hasData()) {
 
-    chart.xAxis.axisLabel('');
+      var series = [];
 
-    chart.xAxis.tickFormat(function(d) {
-      var format = d3.time.format("%B");
-      return format(new Date(d));
-    });
+      var seriesFields = _.filter(results.data().fields, function(i) {
+        return !i.indexOf("_") == 0;
+      });
 
-    chart.xAxis.rotateLabels(45);
+      var data = _.pluck(results.collection().models, 'attributes');
 
-    chart.yAxis.axisLabel('No. of Deployments');
+      _.each(seriesFields, function(f) {
+        var item = {};
+        item.key = f;
+        item.values = [];
+        series.push(item);
+      })
 
-    chart.yAxis.tickFormat(d3.format(',.2f'));
+      _.each(series, function(s) {
+        _.each(data, function(item) {
 
+          var t = item["_time"];
+          var val = item[s.key];
 
+          var i = [];
+          i.push(new Date(t));
+          i.push(parseInt(val));
+
+          s.values.push(i);
+        });
+      });
+
+      var colors = d3.scale.category20();
+
+      var keyColor = function(d, i) {
+        return colors(d.key)
+      };
+
+      var chart;
+
+      nv.addGraph(function() {
+        chart = nv.models.stackedAreaChart()
+          .useInteractiveGuideline(true)
+          .x(function(d) {
+            return d[0];
+          })
+          .y(function(d) {
+            return d[1];
+          })
+          .color(keyColor)
+          .duration(300);
+
+        chart.yAxis.tickFormat(function(d) {
+          return d3.format('d')(d);
+        });
+
+        chart.xAxis.tickFormat(function(d) {
+          return d3.time.format('%w')(new Date(d))
+        });
+
+        chart.legend.vers('furious');
+
+        d3.select('#last3monthsdeploymentsChart')
+          .datum(series)
+          .transition()
+          .duration(1000)
+          .call(chart)
+          .each('start', function() {
+            setTimeout(function() {
+              d3.selectAll('#last3monthsdeploymentsChart *').each(function() {
+                if (this.__transition__)
+                  this.__transition__.duration = 1;
+              })
+            }, 0)
+          });
+        nv.utils.windowResize(chart.update);
+        return chart;
+      });
+
+    }
   });
 
 });
