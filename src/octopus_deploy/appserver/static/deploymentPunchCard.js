4f3a36f2ddf01db9c5547683ec9311f6c1965726
Matthew Erbs <matthew.erbs@merbla.com>
2015-07-14 21:39:11 +1000

    Added Saved Seaches

--- src/octopus_deploy/appserver/static/deploymentPunchCard.js
+++ src/octopus_deploy/appserver/static/deploymentPunchCard.js
@@ -23,26 +23,44 @@ require([
   "d3",
   "calheatmap"
 ], function(_, moment, ready, simpleXmlReady, searchManager, d3, calheatmap) {
+// /search = sourcetype="octopus:deployment" | bucket _time span=1d | stats count by _time
 
-// require("d3");
-//   require("calheatmap");
-
-  var mainSearch = splunkjs.mvc.Components.getInstance("deploymentByDay");
-
+var mainSearch = new searchManager({
+  id: "deploymentsByHourSearch",
+    search: "sourcetype=octopus:deployment | bucket _time span=1d | stats count by _time",
+});
+  // var mainSearch = splunkjs.mvc.Components.getInstance("deploymentByDay");
   var results = mainSearch.data("preview", {});
 
+  results.on("data", function() {
+      if (results.hasData()) {
 
-  var calendar = new CalHeatMap();
-  calendar.init({
-    domain : "month",
-  	subDomain : "x_day",
-  	range : 3,
-  	cellsize: 15,
-  	cellpadding: 3,
-  	cellradius: 5,
-  	domainGutter: 15,
-  	weekStartOnMonday: 0,
-  	scale: [40, 60, 80, 100]
+        var series = [];
 
-  });
+        var data = _.pluck(results.collection().models, 'attributes');
+
+        //
+        // _time,
+        // count
+        _.each(data, function(i) {
+          i.order = parseInt(i.durationInSecs);
+        });
+
+        var calendar = new CalHeatMap();
+        calendar.init({
+          //  data: "datas-years.json",
+          domain: "month",
+          subDomain: "x_day",
+          range: 3,
+          cellsize: 15,
+          cellpadding: 3,
+          cellradius: 5,
+          domainGutter: 15,
+          weekStartOnMonday: 0,
+          scale: [40, 60, 80, 100]
+
+        });
+      }
+    }
+  );
 });
