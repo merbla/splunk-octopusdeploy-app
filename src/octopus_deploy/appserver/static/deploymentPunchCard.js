605a7d15b0f1c068a6be040efd62cd35505015ea
Matthew Erbs <matthew.erbs@merbla.com>
2015-07-14 21:14:08 +1000

    RequireJs with Cal-HeatMap

--- src/octopus_deploy/appserver/static/deploymentPunchCard.js
+++ src/octopus_deploy/appserver/static/deploymentPunchCard.js
@@ -1,26 +1,18 @@
 require.config({
   paths: {
-    lodash: '../app/octopus_deploy/bower_components/lodash/lodash.min',
-    d3v3: '../app/octopus_deploy/bower_components/d3/d3.min',
-    nvd3: '../app/octopus_deploy/bower_components/nvd3/build/nv.d3.min',
+    d3: '../app/octopus_deploy/bower_components/d3/d3.min',
+    calheatmap: '../app/octopus_deploy/bower_components/cal-heatmap/cal-heatmap',
   },
   shim: {
-    lodash: {
-      deps: []
-    },
     d3v3: {
       deps: []
     },
-    nvd3: {
-      deps: ['d3v3']
+    calheatmap: {
+      deps: ['d3']
     }
   }
 });
 
-// workaround for nvd3 using global d3
-define('d3.global', ['d3v3'], function(_) {
-  d3 = _;
-});
 
 require([
   'underscore',
@@ -28,106 +20,29 @@ require([
   "splunkjs/ready!",
   "splunkjs/mvc/simplexml/ready!",
   "splunkjs/mvc/searchmanager",
-  "d3v3",
-  "nvd3",
-  "lodash"
-], function(_, moment, ready, simpleXmlReady, searchManager, d3, nv1, __) {
+  "d3",
+  "calheatmap"
+], function(_, moment, ready, simpleXmlReady, searchManager, d3, calheatmap) {
 
-  //Load v3
-  require("nvd3");
+// require("d3");
+//   require("calheatmap");
 
-  var mainSearch = new searchManager({
-    id: "punchCardSearch",
-      search: "sourcetype=octopus:task Name=Health | timechart count(IsCompleted) by State",
-  });
+  var mainSearch = splunkjs.mvc.Components.getInstance("deploymentByDay");
 
   var results = mainSearch.data("preview", {});
 
-  results.on("data", function() {
-    if (results.hasData()) {
 
-      var series = [];
+  var calendar = new CalHeatMap();
+  calendar.init({
+    domain : "month",
+  	subDomain : "x_day",
+  	range : 3,
+  	cellsize: 15,
+  	cellpadding: 3,
+  	cellradius: 5,
+  	domainGutter: 15,
+  	weekStartOnMonday: 0,
+  	scale: [40, 60, 80, 100]
 
-      var seriesFields = _.filter(results.data().fields, function(i) {
-        return !i.indexOf("_") == 0;
-      });
-
-      var data = _.pluck(results.collection().models, 'attributes');
-
-      _.each(seriesFields, function(f) {
-        var item = {};
-        item.key = f;
-        item.values = [];
-        series.push(item);
-      })
-
-      _.each(series, function(s) {
-        _.each(data, function(item) {
-
-          var t = item["_time"];
-          var val = item[s.key];
-
-          var i = [];
-          i.push(new Date(t));
-          i.push(parseInt(val));
-
-          s.values.push(i);
-        });
-      });
-
-      var colors = d3.scale.category20();
-
-      var keyColor = function(d, i) {
-        return colors(d.key)
-      };
-
-      var chart;
-
-      var colors = d3.scale.category20();
-
-      var keyColor = function(d, i) {
-        return colors(d.key)
-      };
-
-      var chart;
-
-      nv.addGraph(function() {
-        chart = nv.models.lineChart()
-          .options({
-            transitionDuration: 300,
-            useInteractiveGuideline: true
-          });
-        chart
-          .x(function(d) {
-            return d[0];
-          })
-          .y(function(d) {
-            return d[1];
-          });
-
-        chart.xAxis
-          .showMaxMin(false);
-
-        chart.yAxis
-          .axisLabel("Health Checks");
-
-        chart.yAxis.tickFormat(function(d) {
-          return d3.format('d')(d);
-        });
-
-        chart.xAxis.tickFormat(function(d) {
-        return d3.time.format('%B-%d')(new Date(d))
-        });
-
-        d3.select('#tentacleHealthChart')
-          .datum(series)
-          .call(chart);
-
-        nv.utils.windowResize(chart.update);
-
-        return chart;
-      });
-    }
   });
-
 });
