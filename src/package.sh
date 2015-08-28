57988606db7df6df2a4fdf286e4dfddfe4f3146c
Matthew Erbs <matthew.erbs@merbla.com>
2015-08-29 09:31:46 +1000

    1.0.0.1 Cleanup

--- src/package.sh
+++ src/package.sh
@@ -1,7 +1,18 @@
+#Copy to a temp folder
+rm ./../../octopus_deploy
+
+cp -r ./octopus_deploy ./../../octopus_deploy
+
+cd ./../../octopus_deploy
+
+#remove Git Files
+find . -name ".DS_Store" -print0 | xargs -0 rm -rf
+find . -name "._*" -print0 | xargs -0 rm -rf
+
 rm octopus_deploy.tar.gz
 tar -czf octopus_deploy.tar.gz octopus_deploy/
+ls
+
 
 # Remember.... chmod -R a+rX *
-
-
 #cd var/lib/splunk/modinputs/octopus_deploy/
